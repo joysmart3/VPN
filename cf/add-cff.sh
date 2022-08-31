ca8e099cd5ca9ca8ee4380f27e9f21540d415267
joysmart3 <112532260+joysmart3@users.noreply.github.com>
2022-08-31 12:19:38 +0800

    Add files via upload

---
+++ cf/add-cff.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
+clear
+echo "        Add ID Cloudlare "
+echo ""
+read -e -p " Masukan Domain :$domain" domain
+read -e -p " Masukan Email Cloudflare :" email
+read -e -p " Masukan Api Key :" key
+echo -e "domain=$domain" >> /root/mail.conf
+echo -e "email=$email" >> /root/mail.conf
+echo -e "key=$key" >> /root/mail.conf
+clear
+echo "Done"
+echo "Your ID Cloudflare"
+echo -e "==============================="
+echo "DOMAIN         : $domain"
+echo "Email          : $email"
+echo "Api Key        : $key"
+echo -e "==============================="
